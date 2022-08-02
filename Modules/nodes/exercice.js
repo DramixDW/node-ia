456ea9cae7d6266f8cd51268af8d32a599ac3537
DramixDW <verliefden.romain@gmail.com>
2022-08-02 16:56:32 +0200

    mardi soir

--- Modules/nodes/exercice.js
+++ Modules/nodes/exercice.js
@@ -1,5 +1,18 @@
 /**
  * 📌 Installez "image-js"
- * 📌 Faites un programme qui lit 'cat.jpeg' et la tourne de 180 degrés et lui applique une dimension de 200 pixels.
+ * 📌 Faites un programme qui lit 'cat.jpeg' et la tourne de 180 degrés et
+ *  lui applique une dimension de 200 pixels.
  * 📌 Sauvegardez ensuite l'image sous 'cat.png'
  */
+
+const { Image } = require('image-js');
+
+
+execute().catch(console.error);
+async function execute() {
+    let image = await Image.load('cat.jpeg');
+    image = image
+                .resize({width: 200})
+                .rotate(180)
+    return image.save('cat.png');
+}
